6e16db3579149586e688cfff5211683b7d00e502
sanjay tumu <144722861+TumuSanjay000@users.noreply.github.com>
2023-12-15 20:32:32 +0530

    Add files via upload

---
+++ task5.js
@@ -0,0 +1,23 @@
+<script>
+            function add(n1,n2) {
+                console.log(n1+n2);
+            }
+
+            console.log("wellcome to task 5");
+
+
+            let n=15;
+            console.log(n+20);
+
+
+            let i=7;
+            if(i<=n){
+                console.log("i is less than n");
+            }
+            else{
+                console.log("i is grater than n");
+            }
+
+
+            add(n,i);
+</script>
